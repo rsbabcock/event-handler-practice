77bfb1433a866a51a76228993d247f273ddc4018
nss2 <nss2@NSS2ss-MacBook-Pro.local>
2018-04-24 13:27:59 -0500

    added first pass at event handling

---
+++ powers.js
@@ -0,0 +1,45 @@
+
+/*
+    In the following code, replace `event` with the correct string to handle
+    a click event on the button. Then write the function that has instructions
+    for activating the corresponding power. The function should...
+        1. Remove the `disabled` class
+        2. Add the `enabled` class
+
+*/
+
+
+const toggleClass = (section) => {
+    console.log("hi")
+    // for(let i = 0; i< sectionQuery.length; i ++) 
+        console.log(section.classList)
+        
+        // 1. Remove the `disabled` class
+        section.classList.toggle("disabled")
+        // 2. Add the `enabled` class
+        section.classList.add("enabled")
+        // sectionQuery.classList.add("enabled")
+        console.log("after", section.classList)
+        
+    // }
+}
+
+const flightSection = document.querySelector('#flight')
+const mindSection = document.querySelector("#mindreading")
+const xRaySection = document.querySelector("#xray")
+
+document.querySelector("#activate-flight").addEventListener("click", () => toggleClass(flightSection))
+document.querySelector("#activate-mindreading").addEventListener("click", () => toggleClass(mindSection))
+document.querySelector("#activate-xray").addEventListener("click", () => toggleClass(xRaySection))
+
+/*
+    Now write two more event handlers that activate the other two powers
+    when the corresponding buttons are clicked.
+*/
+
+
+/*
+    Write two more event handlers for activating and deactivating all powers
+    when the corresponding buttons are clicked. You will need to use the
+    `document.querySelectorAll()` method for these.
+*/
